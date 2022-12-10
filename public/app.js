706697b61699ff26569cd49a925a42a6ded4bbfc
Alexei Rusu <rusualexei1995@gmail.com>
2022-12-09 22:22:22 -0500

    styled posters on the home route

--- public/app.js
+++ public/app.js
@@ -5,9 +5,10 @@
 // changing the background color of the status based on the current value
 for(item of statuss) {
     // console.log(item.innerHTML)
-    if(item.innerHTML.match("Lost")){
-       console.log('yee boy')
+    if(item.innerHTML.match("LOST")){
+    //    console.log('yee boy')
        item.style.backgroundColor = "red"
+       
     } else {
         item.style.backgroundColor = "green"
     }

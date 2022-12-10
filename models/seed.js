50dd57b35b1fd82f5d2a13090032bfaf59c6f6cc
Alexei Rusu <rusualexei1995@gmail.com>
2022-12-09 19:42:23 -0500

    status background/index styling

--- models/seed.js
+++ models/seed.js
@@ -9,7 +9,7 @@ mongoose.connection.on('open', () => {
 
     // array of starter posters
     const existingPosters = [
-        {name: "Brian", location: "Quahog, Rhode Island", type: "Dog", sex: "Male", details: "went clubbing, never came back", number: 412426252725222, status: "Lost", img: "https://upload.wikimedia.org/wikipedia/en/1/12/Brian_Griffin.png" },
+        {name: "Brian", location: "Quahog, Rhode Island", type: "Dog", sex: "Male", details: "went clubbing, never came back", number: 412426252725222, status: "Lost", img: "https://cdnph.upi.com/svc/sv/i/3121387175078/2013/1/13871785787819/Family-Guy-brings-Brian-Griffin-back-from-the-dead.jpg" },
         {name: "Snowball", location: "New York City, Sewage", type: "Rabbit", sex: "Male", details: "went to war", number: 6252725222, status: "Lost", img: "https://i.pinimg.com/736x/6e/b1/b4/6eb1b4b68b615af502bc15d6af3ef9ea--movies-of--pet-rabbit.jpg" }
     ]
 
---
+++ public/app.js
@@ -0,0 +1,16 @@
+
+ const statuss = document.querySelectorAll(".status")
+
+ 
+// changing the background color of the status based on the current value
+for(item of statuss) {
+    // console.log(item.innerHTML)
+    if(item.innerHTML.match("Lost")){
+       console.log('yee boy')
+       item.style.backgroundColor = "red"
+    } else {
+        item.style.backgroundColor = "green"
+    }
+}
+
+

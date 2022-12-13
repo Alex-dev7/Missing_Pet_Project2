ee51eb9375080d894a8ac922088fb68478db4700
Alexei Rusu <rusualexei1995@gmail.com>
2022-12-13 18:15:35 -0500

    final commit

--- public/app.js
+++ public/app.js
@@ -1,7 +1,5 @@
 
  const statuss = document.querySelectorAll(".status")
-
- 
 // changing the background color of the status based on the current value
 for(item of statuss) {
     // console.log(item.innerHTML)
@@ -14,4 +12,23 @@ for(item of statuss) {
     }
 }
 
+
+// hamburger menu 
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+// event listener to toggle the class active
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+    
+})
+
+// event listener to remove the class active
+document.querySelectorAll(".nav-link").forEach(item => {
+    item.addEventListener("click", () => {
+        hamburger.classList.remove("active")
+        navMenu.classList.remove("active")
+    })
+})
 
